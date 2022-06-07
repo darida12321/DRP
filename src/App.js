f1c73ac2735c6ffad8cc0aaf47726b82c4403da0
dm1920 <dm1920@ic.ac.uk>
2022-06-07 17:59:54 +0100

    Add color scheme to the css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>Hello</h1>
+      <div className="g0 box"></div>
+      <div className="g1 box"></div>
+      <div className="g2 box"></div>
+      <div className="g3 box"></div>
+      <div className="g4 box"></div>
+    </div>
   );
 }
 
