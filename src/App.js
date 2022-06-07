20645f0a2707a4279784d5efdef2e9949ffb37d2
dm1920 <dm1920@ic.ac.uk>
2022-06-07 21:22:23 +0100

    Create an initial static code editor website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React from 'react';
-import img from './images/user-icon.svg'
+import gearImg from './images/gear-icon.svg'
+import houseImg from './images/house-icon.svg'
+import keyboardImg from './images/keyboard-icon.svg'
+import moonImg from './images/moon-icon.svg'
+import userImg from './images/user-icon.svg'
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <div className="g0 box"></div>
-      <div className="g1 box"></div>
-      <div className="g2 box"></div>
-      <div className="g3 box"></div>
-      <div className="g4 box"></div>
-      <img src={img} alt="user"/>
+    <div class="container">
+      <nav>
+        <div>
+          <img src={houseImg} class="icon" alt="" />
+        </div>
+        <div>
+          <img src={keyboardImg} class="icon" alt="" />
+          <h1>Website name</h1>
+        </div>
+        <div>
+          <img src={moonImg} class="icon" alt="" />
+          <img src={gearImg} class="icon" alt="" />
+          <img src={userImg} class="icon" alt="" />
+        </div>
+      </nav>
+      <div className="content">
+        <div className="chapter">
+          <h1>Chapter 1:</h1>
+          <h3>Basic motions</h3>
+
+          <div>
+            <div className="lesson-marker"></div>
+            <p>Introduction to vim</p>
+          </div>
+          <div>
+            <div className="lesson-marker"></div>
+            <p>Moving a word forward</p>
+          </div>
+          <div>
+            <div className="lesson-marker"></div>
+            <p>Moving a word backward</p>
+          </div>
+          <div>
+            <div className="lesson-marker"></div>
+            <p>WORD vs. word</p>
+          </div>
+          <div>
+            <div className="lesson-marker"></div>
+            <p>Move to end of word</p>
+          </div>
+          <div>
+            <div className="lesson-marker"></div>
+            <p>Final test</p>
+          </div>
+
+        </div>
+        <div className="tutorial">
+          <div className="textbox">
+            <div>
+              <h1>Lesson 3: Moving a word back.</h1>
+              <p>Use b to go back an entire word.</p>
+            </div>
+            <div className="marker">1/5</div>
+          </div>
+          <div className="editor"></div>
+        </div>
+      </div>
     </div>
   );
 }
