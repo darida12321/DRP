f3b7e2f191a56465d368a940f23a4576d96579be
dm1920 <dm1920@ic.ac.uk>
2022-06-08 16:13:12 +0100

    Get the chapter and lesson data from the url

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import NavBar from './NavBar'
 import ChapterView from './ChapterView'
 import TutorialWindow from './TutorialWindow'
 
 function Tutorial() {
+  const { chapter, lesson } = useParams();
+  console.log(chapter, lesson)
+
   return (
-    <div class="container">
+    <div className="container">
       <NavBar />
       <ChapterView />
       <TutorialWindow />
