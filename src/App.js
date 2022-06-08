5f368e5144f682c00d2ced503d591daaea11df98
dm1920 <dm1920@ic.ac.uk>
2022-06-08 16:01:09 +0100

    Use routers to get the chapter and lesson from the link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Tutorial from './components/Tutorial'
 
 function App() {
   return (
-    <Tutorial />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/vim/:chapter/:lesson' element={<Tutorial />}/>
+        <Route path='*' element={<h1>404 not found</h1>}/>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
