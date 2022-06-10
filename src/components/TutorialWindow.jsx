e4cb8154f662bf6380977f04673a085d8a3a27a5
dm1920 <dm1920@ic.ac.uk>
2022-06-10 13:15:45 +0100

    Refactor CodeChecker to its own file

--- src/components/TutorialWindow.jsx
+++ src/components/TutorialWindow.jsx
@@ -8,149 +8,16 @@ import "ace-builds/src-noconflict/theme-chaos";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/keybinding-vim";
 import ace from "ace-builds/src-noconflict/ace";
+import CodeChecker from '../js/CodeChecker';
 
 // TODO fetch data from a backend
 async function getLessonData() {
   const querySnapshot = await getDocs(collection(db, "vim/chapter1/lessons"));
   let lessonData = [];
   querySnapshot.forEach((doc) => {
-    console.log(doc.data());
     lessonData.push(doc.data());
   });
   return lessonData;
-
-  // return {
-  //   lesson: {
-  //     num: 1,
-  //     title: "Moving with hjkl",
-  //     desc: "Use the H, J, K and L keys to move to the target.\n'H' - left, 'J' - down, 'K' - up, 'L' - right",
-  //     exampleCount: 3,
-  //   },
-  //   examples: [
-  //     {
-  //       initial: {
-  //         code: "                      |\n  Move here: --> <--  |\n                |",
-  //         cLine: 0,
-  //         cPos: 0,
-  //       },
-  //       expected: {
-  //         cLine: 1,
-  //         cPos: 14,
-  //       },
-  //     },
-  //     {
-  //       initial: {
-  //         code: "       Now here: > <   |\n                       |\n                       |",
-  //         cLine: 1,
-  //         cPos: 14
-  //       },
-  //       expected: {
-  //         cLine: 0,
-  //         cPos: 18,
-  //       },
-  //     },
-  //     {
-  //       initial: {
-  //         code: "                          |\n                          |\n   > < and finally, there |",
-  //         cLine: 0,
-  //         cPos: 18,
-  //       },
-  //       expected: {
-  //         cLine: 2,
-  //         cPos: 4,
-  //       },
-  //     },
-  //   ],
-  // };
-}
-
-class CodeChecker {
-  // Constructor defining the parameters
-  // TODO: pass parameters in here (editor, examples, callback)
-  // TODO: set the editor to examples[0] setup
-  constructor(exampleCount) {
-    this.editor = null;
-
-    this.desiredState = null;
-
-    this.examples = [];
-    this.exampleNum = 0;
-    this.exampleCount = exampleCount;
-
-    this.callback = null;
-  }
-
-  setEditor(editor) {
-    this.editor = editor;
-  }
-
-  // TODO: Use official Lesson json format
-  setEditorState(state) {
-    this.editor.setValue(state.code);
-    this.editor.moveCursorTo(state.cLine, state.cPos);
-    this.editor.session.selection.clearSelection();
-  }
-
-  setExamples(examples) {
-    this.examples = examples;
-  }
-
-  setDesiredState(desired) {
-    this.desiredState = desired;
-  }
-
-  setCallback(f) {
-    this.callback = f;
-  }
-
-  // TODO use official lessonData Json format
-  getEditorState() {
-    if (!this.editor) {
-      return null;
-    }
-    const code = this.editor.getValue();
-    const cursor = this.editor.getCursorPosition();
-    return { code: code, line: cursor.row, pos: cursor.column };
-  }
-
-  // Use example instead of desired state
-  goalReached() {
-    if (this.desiredState === null) {
-      return false;
-    }
-    const state = this.getEditorState();
-
-    return state.line === this.desiredState.line && state.pos === this.desiredState.pos;
-  }
-
-  // Use callback for current excersize
-  codeUpdated() {
-    if (this.callback && this.goalReached()) {
-      this.incrementExample();
-    }
-    return this.exampleNum;
-  }
-
-  // Use correct json format
-  incrementExample() {
-    if (this.exampleNum >= this.exampleCount) {
-      return;
-    }
-    this.exampleNum += 1;
-    if (this.exampleNum < this.exampleCount) {
-      this.setDesiredState({
-        line: this.examples[this.exampleNum].expected.cLine,
-        pos: this.examples[this.exampleNum].expected.cPos,
-      });
-      this.setEditorState({
-        code: this.examples[this.exampleNum].initial.code,
-        cLine: this.examples[this.exampleNum].initial.cLine,
-        cPos: this.examples[this.exampleNum].initial.cPos,
-      });
-    } else {
-      this.callback();
-    }
-  }
 }
 
 function TutorialWindow() {
@@ -165,29 +32,16 @@ function TutorialWindow() {
       setLesson({
         num: data[0].num,
         title: data[0].title,
-        desc: data[0].desc,
-        exampleCount: data[0].exampleCount,
-      });
-      codeChecker.current = new CodeChecker(data[0].exampleCount);
-      codeChecker.current.setEditor(ace.edit("editor"));
-      codeChecker.current.setExamples(data[0].examples);
-
-      codeChecker.current.setEditorState({
-        code: data[0].examples[0].initial.code,
-        cLine: data[0].examples[0].initial.cLine,
-        cPos: data[0].examples[0].initial.cPos,
+        description: data[0].description,
+        exampleCount: data[0].examples.length,
       });
 
-      console.log("EXAMPLE", data[0].examples[0])
-      codeChecker.current.setDesiredState({
-        line: data[0].examples[0].expected.cLine,
-        pos: data[0].examples[0].expected.cPos,
-      });
-
-      codeChecker.current.setCallback(() => {
-        console.log("Lesson done!!!!");
+      codeChecker.current = new CodeChecker(
+          ace.edit('editor'), data[0].examples, () => {
+        console.log('Lesson done!!!')
         setComplete(true);
       });
+
     }
     fetchData();
   }, []);
@@ -212,7 +66,7 @@ function TutorialWindow() {
   }
 
   function onCursorChange(selection) {
-    if (editing) {
+    if (editing || !codeChecker.current) {
       return;
     }
     setExampleNum(codeChecker.current.codeUpdated());
@@ -228,10 +82,10 @@ function TutorialWindow() {
           <h1>
             Lesson {lesson.num}: {lesson.title}
           </h1>
-          <p>{lesson.desc}</p>
+          <p>{lesson.description}</p>
         </div>
         <div>
-          <div className="marker">
+          <div className="marker" style={{ background: (complete ? boxShadowComplete : boxShadowDefault)}}>
             {exampleNum}/{lesson.exampleCount}
           </div>
         </div>
