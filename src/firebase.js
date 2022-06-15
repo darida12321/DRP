6182e6f4aff10322dedba9fa8c2584dcde301b86
dks20 <dks20@ic.ac.uk>
2022-06-15 14:39:01 +0100

    fully overwrite documents when setting

--- src/firebase.js
+++ src/firebase.js
@@ -42,7 +42,7 @@ export async function getChapterData(chapterNum) {
 
 export async function submitLesson(chapterNum, lessonNum, lessonObj) {
   const docRef = doc(db, "vim/chapter" + chapterNum + "/lessons", "lesson" + lessonNum);
-  await setDoc(docRef, lessonObj, { merge: true });
+  await setDoc(docRef, lessonObj);
   console.log("submitted chapter: " + chapterNum + ", lesson: " + lessonNum);
 }
 
