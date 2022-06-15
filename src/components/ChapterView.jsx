dee99eda3799f984a271954fdf16b9d92328780e
dm1920 <dm1920@ic.ac.uk>
2022-06-15 16:57:38 +0100

    Change chapter links so they reload the page

--- src/components/ChapterView.jsx
+++ src/components/ChapterView.jsx
@@ -30,7 +30,6 @@ function ChapterView(props) {
     <div id="chapter">
       <h1>Chapter {props.chapter}</h1>
       <h3>{lessons.title && lessons.title}</h3>
-
       <div id="lessons-view">
         {lessons.lessonData &&
           lessons.lessonData.map((l, i) => (
@@ -39,10 +38,12 @@ function ChapterView(props) {
                 <div className="chapter-blob" />
                 <div className="chapter-line" />
               </div>
-
-              <Link to={`/vim/${props.chapter}/${i + 1}`} className="link">
-                <p className="lesson-title">{l}</p>
-              </Link>
+              <a
+                onClick={() => {window.location.href=`/vim/${props.chapter}/${i + 1}`}}
+                className='link lesson-title'
+              >
+                {l}
+              </a>
             </div>
           ))}
       </div>
