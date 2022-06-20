6ecb0cf6c9a0af5b754795ffdec4c8b5a23e3dac
Architect Jr <fh720@doc.ic.ac.uk>
2022-06-20 16:49:10 +0100

    altered aesthetic of homepage + added NavBar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SubjectBox from './SubjectBox';
+import NavBar from './NavBar';
 
 import '../styles/homePage.css';
 import MoonImg from '../images/moon-icon.svg';
-
-function startUpClicked() {
-  console.log('pressed start up button');
-}
 
 function HomePage() {
 
   return (
     <div className = "main">
+      <NavBar/>
       <div className = "banner">
         <h1>Website Name</h1>
-        <div className = "loginBoxes">
-          <Link to='/vim/1/1'>
-            <button onClick = {startUpClicked}>
-              <p>Get Started</p>
-            </button>
-          </Link>
-        </div>
       </div>
       <div className = 'body'>
-        <h2>Content</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum exercitationem maxime magni impedit aperiam. Dolores et consectetur reiciendis velit hic sit, voluptatum consequatur necessitatibus cum laborum obcaecati aliquam exercitationem deserunt.</p>
-        <SubjectBox link='/' subject='whatever' image={MoonImg}/>
+        <div className = 'bodyContent'>
+          <h2>Content</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum exercitationem maxime magni impedit aperiam. Dolores et consectetur reiciendis velit hic sit, voluptatum consequatur necessitatibus cum laborum obcaecati aliquam exercitationem deserunt.</p>
+        </div>
+      </div>
+      <div className='courses'>
+        <h1>Try our Free Courses!</h1>
+          <div className='tutorialBoxes'>
+            <SubjectBox link='/' subject='whatever' image={MoonImg} about='sussy'/>
+          </div>
       </div>
     </div>
   );
